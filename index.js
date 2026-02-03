7df9a1bbaa58109d49fff2a1633363de56b36812
Muhammad Umair <m.umaircomputer381786@gmail.com>
2026-02-03 19:24:31 +0500

    fix css issue

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ connectToMongoDB(process.env.MONGO_URL)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static("public"))
+app.use(express.static(path.join(__dirname, "public")))
 
 app.set("view engine", 'ejs')
 app.set("views", path.resolve("./views"))
